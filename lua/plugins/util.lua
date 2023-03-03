b7325bb6a60de7e34fe14291763ee983d0125b7c
scimania <gauravdeepubhi@gmail.com>
2023-03-04 00:28:52 +0530

    added mini.pairs

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -1,4 +1,12 @@
 return {
-  { "nvim-lua/plenary.nvim", lazy = true },
-  { "nvim-tree/nvim-web-devicons", lazy = true },
+	{
+		"echasnovski/mini.pairs",
+		version = false,
+		event = { "BufReadPre", "BufNewFile" },
+		config = function(_, opts)
+			require("mini.pairs").setup(opts)
+		end,
+	},
+	{ "nvim-lua/plenary.nvim", lazy = true },
+	{ "nvim-tree/nvim-web-devicons", lazy = true },
 }
