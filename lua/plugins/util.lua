0672d4bb10563ccf0e400e5bd7786cc80b615467
scimania <gauravdeepubhi@gmail.com>
2023-03-04 12:50:51 +0530

    added undotree

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -15,50 +15,56 @@ return {
 			require("mini.surround").setup(opts)
 		end,
 	},
-  {
-    'numToStr/Comment.nvim',
+	{
+		"numToStr/Comment.nvim",
 		event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        require('Comment').setup()
-    end
-  },
-  {
-    "RRethy/vim-illuminate",
-    event = { "BufReadPost", "BufNewFile" },
-    opts = { delay = 200 },
-    config = function(_, opts)
-      require("illuminate").configure(opts)
+		config = function()
+			require("Comment").setup()
+		end,
+	},
+	{
+		"mbbill/undotree",
+		keys = {
+			{ "<leader>u", "<cmd>UndotreeToggle<cr>", desc = "open Undo Tree" },
+		},
+	},
+	{
+		"RRethy/vim-illuminate",
+		event = { "BufReadPost", "BufNewFile" },
+		opts = { delay = 200 },
+		config = function(_, opts)
+			require("illuminate").configure(opts)
 
-      local function map(key, dir, buffer)
-        vim.keymap.set("n", key, function()
-          require("illuminate")["goto_" .. dir .. "_reference"](false)
-        end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
-      end
+			local function map(key, dir, buffer)
+				vim.keymap.set("n", key, function()
+					require("illuminate")["goto_" .. dir .. "_reference"](false)
+				end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
+			end
 
-      map("]]", "next")
-      map("[[", "prev")
+			map("]]", "next")
+			map("[[", "prev")
 
-      -- also set it after loading ftplugins, since a lot overwrite [[ and ]]
-      vim.api.nvim_create_autocmd("FileType", {
-        callback = function()
-          local buffer = vim.api.nvim_get_current_buf()
-          map("]]", "next", buffer)
-          map("[[", "prev", buffer)
-        end,
-      })
-    end,
-    keys = {
-      { "]]", desc = "Next Reference" },
-      { "[[", desc = "Prev Reference" },
-    },
-  },
-  {
-    'lewis6991/gitsigns.nvim',
-    event = { "BufReadPre", "BufNewFile" },
-    config = function (_, _)
-      require("gitsigns").setup()
-    end
-  },
+			-- also set it after loading ftplugins, since a lot overwrite [[ and ]]
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function()
+					local buffer = vim.api.nvim_get_current_buf()
+					map("]]", "next", buffer)
+					map("[[", "prev", buffer)
+				end,
+			})
+		end,
+		keys = {
+			{ "]]", desc = "Next Reference" },
+			{ "[[", desc = "Prev Reference" },
+		},
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function(_, _)
+			require("gitsigns").setup()
+		end,
+	},
 	{ "nvim-lua/plenary.nvim", lazy = true },
 	{ "nvim-tree/nvim-web-devicons", lazy = true },
 }
